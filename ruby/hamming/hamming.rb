7567db54bbfc6be2b7f52891750de35d31a23e62
twittke <twittke@mi.fu-berlin.de>
2017-03-29 20:48:19 +0200

    Satisfy all but two assertions (ArgumentError expected)

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,15 @@
+module BookKeeping
+  VERSION = 3
+end
+
 class Hamming
   def self.compute(str_a, str_b)
     if str_a.length == str_b.length
       result = 0
       for i in (0..str_a.length)
-        if str_a[i] != str_b[i]
-          result+=1
-          puts result
-        end
-      return result
+        result+=1 if str_a[i] != str_b[i]
       end
+	return result
     end
   end
 end
