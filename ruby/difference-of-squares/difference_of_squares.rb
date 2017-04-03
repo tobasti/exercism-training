b12cf66bd7cdcd3166b737ecdbb97dfe62620162
Tobasti <tobasti@riseup.net>
2017-04-03 15:46:51 +0200

    Satisfy all tests in difference-of-squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,36 @@
+class Squares
+  def initialize(arg)
+    @squ_of_sum
+    @sum_of_sqr
+    @difference
+    @base = arg.to_i
+  end
+
+  def square_of_sum
+    if @squ_of_sum
+      return @squ_of_sum
+    else
+      @squ_of_sum = 0
+      (1..@base).each{ |n| @squ_of_sum += n }
+      @squ_of_sum *= @squ_of_sum
+    end
+  end
+
+  def sum_of_squares
+    if @sum_of_squ
+      return @sum_of_squ
+    else
+      @sum_of_squ = 0
+      (1..@base).each{ |n| @sum_of_squ += n*n }
+      @sum_of_squ
+    end
+  end
+
+  def difference
+    square_of_sum - sum_of_squares 
+  end
+end
+
+module BookKeeping
+  VERSION = 3 # Where the version number matches the one in the test.
+end
