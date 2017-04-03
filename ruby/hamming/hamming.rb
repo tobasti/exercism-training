45ce4023b03c892e35c85205716d0874d0037df9
Tobasti <tobasti@riseup.net>
2017-04-03 10:42:47 +0200

    Refactor hamming.rb, 2nd iteration

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,14 +4,18 @@ end
 
 class Hamming
   def self.compute(strand_a, strand_b)
-    unless strand_a.length == strand_b.length
-      raise ArgumentError, "Can't compute. Hamming distance for two strings of \
+
+    error_argument = <<~message_text
+        Can't compute. Hamming distance for two strings of \
         differing length is not defined.\n\
-        Please input two Strings of the same length!"
-    else
-      strand_a.chars.zip(strand_b.chars).count do |element_a, element_b|
-        element_a != element_b
-      end
+        Please input two Strings of the same length!
+    message_text
+
+    raise ArgumentError, error_argument unless strand_a.length == strand_b.length
+    
+    # if all is well
+    strand_a.chars.zip(strand_b.chars).count do |element_a, element_b|
+      element_a != element_b
     end
   end
 end
