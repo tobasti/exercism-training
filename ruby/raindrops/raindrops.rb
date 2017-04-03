46abbef389ab68a4f1dbcdd8a765d89dbe44a89a
Tobasti <tobasti@riseup.net>
2017-04-03 16:07:37 +0200

    Refactor '+=' to '<<' & introduce ternary operator

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -7,14 +7,10 @@ class Raindrops
     number = string.to_i
     output = ''
 
-    output += 'Pling' if number%3==0
-    output += 'Plang' if number%5==0
-    output += 'Plong' if number%7==0
+    output << 'Pling' if number%3==0
+    output << 'Plang' if number%5==0
+    output << 'Plong' if number%7==0
 
-    if output.length == 0
-      number.to_s
-    else
-      output
-    end
+    output.empty? ? number.to_s : output
   end
 end
