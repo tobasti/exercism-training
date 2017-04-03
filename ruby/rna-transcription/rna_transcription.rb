dc69d618f97b8836d13dcc8de3e7ed9f3e40140c
Tobasti <tobasti@riseup.net>
2017-04-03 11:37:59 +0200

    Satisfy all tests in rna_transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -6,7 +6,11 @@ class Complement
 
   COMPLEMENTS = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
 
-  def self.of_dna
+  def self.of_dna(strand)
+    strand.chars.map do |nucleotid|
+      return '' unless COMPLEMENTS.key?(nucleotid)
+      COMPLEMENTS[nucleotid]
+    end.join
   end
 
   def check_input
